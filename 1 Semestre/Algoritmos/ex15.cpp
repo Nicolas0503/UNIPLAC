3d9d619eed0dc9ba74910677527a3b0c6c261c36
Nicolas0503 <162383127+Nicolas0503@users.noreply.github.com>
2024-09-11 17:09:16 -0300

    Create ex15.cpp

---
+++ 1 Semestre/Algoritmos/ex15.cpp
@@ -0,0 +1,27 @@
+//6. Faça um algoritmo para calcular o volume de uma esfera de raio r. Fórmula:
+//V = 4πr3 / 3
+
+//acho que fudeu
+
+
+#include <iostream>
+
+using namespace std;
+main ()
+
+{ 
+    setlocale (LC_ALL, "Portuguese");
+
+    float formula = 3.14159, volume, calculo;
+    
+    cout << " informe o volume da esfera ";
+    cin >> volume;
+
+    calculo = volume / formula;
+
+    cout << " O volume de sua esfera é de " << calculo << endl;
+ 
+    // Tá certo ??? (provavelmente não).
+    // Não tenho idéia de como fazer isso.
+
+}
